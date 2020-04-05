7f7dfce7a663a6c29da61d76d7ee8d34c76b7ced
Kostia <kostyaetherell@gmail.com>
2020-04-05 22:03:51 +0300

    Add a full_title helper

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,12 @@
 class ApplicationController < ActionController::Base
-	def hello
-		rener html: "Hello world!"
+
+	def full_title(page_title='')
+		base_title = "Ruby on Rails Tutorial Sample App"
+		if page_title.empty?
+			base_title
+		else
+			page_title + ' | ' + base_title
+		end
 	end
+
 end
