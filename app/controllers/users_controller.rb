f7239dcf8f4dd8fcd381f10516927e486994c552
Kostia <kostyaetherell@gmail.com>
2020-04-07 23:34:32 +0300

    finish user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,31 @@
 class UsersController < ApplicationController
-  def new
-  end
+	# Показ странички юзера
+	def show
+	    @user = User.find(params[:id])
+	    # debugger
+	end
+
+	# Страничка регистрации
+	def new
+	  	@user = User.new
+	end
+
+	def create
+	    @user = User.new(user_params)    # Не окончательная реализация!
+	    if @user.save
+	       flash[:success] = "Welcome to the Sample App!"
+      		redirect_to @user
+	    else
+	      	render 'new'
+	    end
+	end
+
+	private
+
+	# Приватный метод получающий параметры
+	def user_params
+      params.require(:user).permit(:name, :email, :password,
+                                   :password_confirmation)
+    end
+
 end
