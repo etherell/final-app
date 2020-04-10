5f4e034e451b66f86ea1bdfd0c5e80c6b89e8e40
Kostia <kostyaetherell@gmail.com>
2020-04-10 22:39:58 +0300

    adding pagination

--- app/models/article.rb
+++ app/models/article.rb
@@ -3,4 +3,5 @@ class Article < ApplicationRecord
 	belongs_to :user										# Связывание статьи с юзером
 	validates :title, presence: true, length: { in: 5..70 } # Валидация минимальной длины для заголовка статьи
 	validates :text, presence: true, length: { in: 10..250 }# Масимальная длинна сообщения до 255 символов
+	self.per_page = 10
 end
