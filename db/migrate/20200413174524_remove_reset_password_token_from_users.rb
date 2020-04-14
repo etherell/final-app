8c3b603953dd0ee8d2bb4d2052a06cc619bc7473
Kostia <kostyaetherell@gmail.com>
2020-04-14 12:12:34 +0300

    fixing

--- db/migrate/20200413174524_remove_reset_password_token_from_users.rb
+++
@@ -1,6 +0,0 @@
-class RemoveResetPasswordTokenFromUsers < ActiveRecord::Migration[6.0]
-  def change
-
-    remove_column :users, :reset_password_token, :string
-  end
-end
--- db/migrate/20200413174801_remove_reset_password_sent_at_from_users.rb
+++
@@ -1,6 +0,0 @@
-class RemoveResetPasswordSentAtFromUsers < ActiveRecord::Migration[6.0]
-  def change
-
-    remove_column :users, :reset_password_sent_at, :datetime
-  end
-end
